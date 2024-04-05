c6118aff48331e0c0142b818369095bd30b20835
loehur <loehur@gmail.com>
2024-04-05 20:45:33 +0700

    operan fix2s

--- app/Views/Room/viewer.php
+++ app/Views/Room/viewer.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <title>Chip</title>
     <link rel="stylesheet" href="<?= $this->ASSETS_URL ?>plugins/bootstrap-5.1/bootstrap.min.css" rel="stylesheet" />
     <link rel="icon" type="image/x-icon" href="<?= $this->ASSETS_URL ?>assets/img/favicon.png" />
@@ -20,51 +20,66 @@
         .modal-backdrop {
             opacity: 0.1 !important;
         }
+
+        /* Chrome, Safari, Edge, Opera */
+        input::-webkit-outer-spin-button,
+        input::-webkit-inner-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+        }
     </style>
 </head>
 
-<div id="content"></div>
-<div id="mutasi"></div>
+<body class="bg-light">
+    <audio id="myAudio">
+        <source src="<?= $this->ASSETS_URL ?>audio/coinout.mp3" type="audio/mpeg">
+    </audio>
 
-<!-- Modal -->
-<div class="modal" id="exampleModal">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <form action="<?= $this->BASE_URL ?>Room/transfer" method="POST">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Transfer Chip to<br><b class="text-dark" id="target"></b></h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <input name="c" type="number" style="height: 100px;font-size:50px" class="form-control text-center">
-                    <input name="t" type="hidden">
-                    <div class="row mt-3">
-                        <?php $fast = $this->model("M_DB_1")->get_order("mutasi", "id DESC LIMIT 10");
-                        $no = 0;
-                        $val = [];
-                        foreach ($fast as $fa) {
-                            if ($no >= 2) {
-                                break;
-                            }
-                            if (!isset($val[$fa['chip']])) {
-                                $val[$fa['chip']] = true;
-                                $no += 1;
-                            } else {
-                                continue;
-                            }
-                        ?>
-                            <div class="col">
-                                <span class="btn btn-lg btn-outline-secondary w-100 fastChip"><?= $fa['chip'] ?></span>
-                            </div>
-                        <?php }
-                        ?>
+    <div id="content" class=""></div>
+    <div id="mutasi" class="px-1"></div>
+
+    <!-- Modal -->
+    <div class="modal" id="exampleModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form id="form_submit" action="<?= $this->BASE_URL ?>Room/transfer" method="POST">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Transfer Chip to<br><b class="text-primary" id="target"></b></h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <button type="submit" class="mt-3 py-4 btn btn-lg btn-outline-dark w-100" data-bs-dismiss="modal">Transfer</button>
-                </div>
-            </form>
+                    <div class="modal-body">
+                        <input name="c" type="number" style="height: 100px;font-size:50px;" class="form-control text-center shadow-none">
+                        <input name="t" type="hidden">
+                        <div class="row mt-3">
+                            <?php $fast = $this->model("M_DB_1")->get_order("mutasi", "id DESC LIMIT 10");
+                            $no = 0;
+                            $val = [];
+                            foreach ($fast as $fa) {
+                                if ($no >= 2) {
+                                    break;
+                                }
+                                if (!isset($val[$fa['chip']])) {
+                                    $val[$fa['chip']] = true;
+                                    $no += 1;
+                                } else {
+                                    continue;
+                                }
+                            ?>
+                                <div class="col">
+                                    <span class="btn btn-lg btn-outline-light border w-100 fastChip py-3">
+                                        <h3 class="text-danger fw-bold"><?= $fa['chip'] ?></h3>
+                                    </span>
+                                </div>
+                            <?php }
+                            ?>
+                        </div>
+                        <button type="submit" id="submit" onclick="playAudio()" class="mt-3 py-4 btn btn-lg btn-light border shadow-sm w-100" data-bs-dismiss="modal">Transfer</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-</div>
+</body>
 
 <script src="<?= $this->ASSETS_URL ?>js/jquery-3.7.0.min.js"></script>
 <script src="<?= $this->ASSETS_URL ?>plugins/bootstrap-5.1/bootstrap.bundle.min.js"></script>
@@ -73,6 +88,11 @@
     $(document).ready(function() {
         content();
     });
+
+    function playAudio() {
+        var x = document.getElementById("myAudio");
+        x.play();
+    }
 
     function content() {
         $("div#content").load('<?= $this->BASE_URL ?><?= $data['page'] ?>/content');
@@ -105,6 +125,7 @@
 
     $(".fastChip").click(function() {
         $("input[name=c]").val($(this).html());
+        $("form#form_submit").submit();
     })
 
     const interval = setInterval(function() {
